64e364c895bfa293769b782e1c4fa0ed7d5293af
mariselaor <64675015+mariselaor@users.noreply.github.com>
2024-05-13 15:54:53 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qr-app-ionic',
+  webDir: 'www'
+};
+
+export default config;
